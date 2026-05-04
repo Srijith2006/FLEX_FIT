d4d8e4d29e6c3404be0e610ed1cde93fbe78d8b7
Srijith S <srijith11.2006@gmail.com>
2026-05-04 11:14:51 +0530

    Update App.jsx with Marketplace and Vendor routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,40 @@ import Navbar from "./components/common/Navbar.jsx";
 import ProtectedRoute from "./components/common/ProtectedRoute.jsx";
 import Login from "./components/auth/Login.jsx";
 import Register from "./components/auth/Register.jsx";
+
+// Pages
 import Home from "./pages/Home.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+
+// New Components
+import Marketplace from "./components/common/Marketplace.jsx";
+import VendorDashboard from "./components/vendor/VendorDashboard.jsx";
+import GroupChat from "./components/common/GroupChat.jsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        
+        {/* Marketplace - Accessible to all logged-in users */}
+        <Route path="/marketplace" element={<ProtectedRoute><Marketplace /></ProtectedRoute>} />
+
+        {/* Dashboard & Profile */}
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+
+        {/* Vendor Specific Route */}
+        <Route path="/vendor/dashboard" element={<ProtectedRoute><VendorDashboard /></ProtectedRoute>} />
+
+        {/* Group Chat - Usually accessed via a program ID */}
+        <Route path="/group-chat/:programId" element={<ProtectedRoute><GroupChat /></ProtectedRoute>} />
       </Routes>
     </>
   );
